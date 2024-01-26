c7ec9f46b6c389d76fa666d6d9e75fdb54793039
Murendenimudzusi <113940379+Murendenimudzusi@users.noreply.github.com>
2024-01-26 12:09:32 +0200

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,39 @@
+package com.company;
+
+import java.lang.reflect.Array;
+
+public class Main {
+
+    public static void main(String[] args) {
+        //Question  1
+        int list = [3,6,5,9,];
+
+        public static void multiples(int list) {
+            int sum ;
+
+            sum =  3 +5+6+9 ;
+            System.out.println(sum );
+
+
+        }
+
+
+
+
+}
+    //Question 2
+
+    int num1 = 3 ;
+    int num2 = 4 ;
+    public static void  has_three ( int num1 , int num2) {
+
+        if ((num1 = 3 || num2 = 3) && num1 + num2 = 3) {
+            System.out.println("True");
+        } else {
+            System.out.println("False");
+        }
+
+
+    }
+
+
